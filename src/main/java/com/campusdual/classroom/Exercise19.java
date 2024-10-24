cc16e424d83565733b1618942fd48a73d95970c1
Ricardo Lopez Peña <ricardoslopezs2001@gmail.com>
2024-10-24 14:04:24 +0200

    Java, ejercicio 19. 2 primeros test completados. INCOMPLETO

--- src/main/java/com/campusdual/classroom/Exercise19.java
+++ src/main/java/com/campusdual/classroom/Exercise19.java
@@ -2,7 +2,7 @@ package com.campusdual.classroom;
 
 public class Exercise19 {
 
-    private static String getTridimensionalString(int[][][] intArrayTri, int[][] flatMatrix) {
+   private static String getTridimensionalString(int[][][] intArrayTri, int[][] flatMatrix) {
 
         int rows = intArrayTri[0].length;
         StringBuilder sb = new StringBuilder();
@@ -32,6 +32,7 @@ public class Exercise19 {
         return getUnidimensionalString(flatMatrix[row]);
     }
 
+
     public static String getTridimensionalString(int[][][] intArrayTri) {
         int[][] flatMatrix = flatTridimensionalArray(intArrayTri);
         return getTridimensionalString(intArrayTri, flatMatrix);
@@ -39,37 +40,82 @@ public class Exercise19 {
 
     //TODO
     public static int[][] flatTridimensionalArray(int[][][] intArrayTri) {
-
+    int[][] bidimensional= new int[1][1];
+       return bidimensional;
     }
 
 
     // TODO
+
     public static String getBidimensionalString(int[][] intArrayBi) {
-
+       return " ";
     }
 
     // TODO
-    public static String getUnidimensionalString(int[] uniArray) {
 
+    public static String getUnidimensionalString(int[] uniArray) {
+        return  " ";
     }
+
 
     // TODO
     public static int[] createAndPopulateUnidimensionalArray(int columns) {
+    int [] firstIntArray = new int[columns];
+        int value=0;
+                //posiciones: (0,1,2,3,4)= 5 posiciones
+        //ASIGNAR VALORES A LOS INDICADORES
+        for(int i=0;i<firstIntArray.length;i++){
+            value++;
+            firstIntArray[i]=value;
+        }
 
+        //IMPRIMIR ARRAYS EN HORIZONTAL sout.print
+        for (int i = 0; i < firstIntArray.length;i++){
+
+            System.out.print(firstIntArray[i]+" ");
+        }
+        System.out.println();
+        return firstIntArray;
     }
 
     // TODO
+
+
+
     public static int[][] createAndPopulateBidimensionalArray(int rows, int columns) {
 
+       int [][] ejemplo = new int [rows][columns];
+        int value = 0;
+        //ASIGNAR VALORES
+        for (int i=0; i < rows;i++) {
+            for (int j=0; j<columns;j++){
+                value++;
+                ejemplo[i][j]=value;
+            }
+        }
+        //MOSTRAR LOS VALORES DEL ARRAY
+        for (int i=0; i < rows;i++) {
+            for (int j=0; j<columns;j++){
+                System.out.print(ejemplo[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println("\n");
+    return ejemplo;
     }
 
     // TODO
     public static int[][][] createAndPopulateTridimensionalArray(int depth, int rows, int columns) {
+        int [][][] ejemplo2 = new int [depth][rows][columns];
+        return ejemplo2;
+   }
 
-    }
 
     public static void main(String[] args) {
         int[] uniArray = createAndPopulateUnidimensionalArray(5);
+        System.out.println("=================================");
+        int[][] intArrayBi = createAndPopulateBidimensionalArray(5, 5);
+        /*
         System.out.println(getUnidimensionalString(uniArray));
         System.out.println("===================");
         int[][] intArrayBi = createAndPopulateBidimensionalArray(5, 5);
@@ -77,5 +123,6 @@ public class Exercise19 {
         System.out.println("===================");
         int[][][] intArrayTri = createAndPopulateTridimensionalArray(3, 3, 3);
         System.out.println(getTridimensionalString(intArrayTri));
+        */
     }
 }
